08ae4052683b0d02e27eef56f3ac5e35abe10f09
trangtran234 <tn-trang.tran@outlook.com.vn>
2018-11-23 18:20:05 +0700

    Web API

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private ProductRepository repo = ProductRepository.Current;
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repo.GetAllProducts();
+        } 
+        public IHttpActionResult GetProduct(int id)
+        {
+            var prod = repo.GetProduct(id);
+            if (prod == null)
+            {
+                return NotFound();
+            }
+            return Ok(prod);
+        }
+    }
+}
---
+++ ProductsApp/Models/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class ProductRepository
+    {
+        private static ProductRepository repo = new ProductRepository();
+        public static ProductRepository Current
+        {
+            get { return repo; }
+        }
+        private List<Product> data = new List<Product>
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return data;
+        }
+        public Product GetProduct(int id)
+        {
+            return data.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
